74e92bb7dc042db3a4e1e43ac033bb0377174496
Alex-Preissler <AlexJPreissler@Outlook.com>
2018-08-12 19:11:38 -0400

    server functional

---
+++ app/public/assets/javascript/survey.js
@@ -0,0 +1,39 @@
+var name;
+var photo;
+var answers = [];
+
+$('#sendAnswers').on('click', function(event) {
+    
+    event.preventDefault();
+
+    name = $('#name').val().trim();
+    photo = $('#photo').val().trim();
+
+    for(var i=0; i<10; i++) {
+
+        var id = '#' + i.toString();
+
+        answers[i] = $(id).val().trim();
+        answers[i] = parseInt(answers[i]);
+    }
+
+    var newUser = new userConstructor(name, photo, answers);
+
+    $.post("/api/friends", newUser,
+        function(data) {
+
+            console.log(data);
+          
+        });
+    });
+ 
+    
+
+function userConstructor(name, photo, answers) {
+
+    this.name = name;
+    this.photo = photo;
+    this.scores = answers;
+
+};
+
